04de32ce700b3f7cb67fe9d6223ad94a021dc891
Kamil Pawlowski <kamilpe@gmail.com>
2025-03-13 10:56:02 +0100

    Initial Commit

---
+++ Raycaster-Demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Raycaster-Demo
+//
+//  Created by Kamil Pawlowski on 13/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
